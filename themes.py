a9fc3ba191f3631e370d59a9d17f4db0374a8335
Simone <simone.denada@gmail.com>
2021-10-07 00:51:38 +0200

    theme default-minimal

--- themes.py
+++ themes.py
@@ -9,4 +9,13 @@ themes = {
 
         }
     },
+    "default-minimal": {
+        'max_height': '450px',
+        "available_fields": {
+            "invert_author_title": False,
+            "toggle_song_progress_bar": True,
+            "toggle_sound_waves": True,
+
+        }
+    },
 }
